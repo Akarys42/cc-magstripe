f7cf3a0020c3abaa1906aa3ed7d94c1e8c1394d8
Amber Bertucci <amber@akarys.me>
2022-07-04 13:50:18 +0200

    Add basic card reader block

--- src/main/java/me/akarys/ccmagstripe/CCMagstripeMod.java
+++ src/main/java/me/akarys/ccmagstripe/CCMagstripeMod.java
@@ -1,21 +1,25 @@
 package me.akarys.ccmagstripe;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CCMagstripeMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("ccmagstripe");
+
+	public static final Block CARD_READER = new Block(FabricBlockSettings.of(Material.METAL).strength(2f).requiresTool());
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.BLOCK, new Identifier("ccmagstripe", "card_reader"), CARD_READER);
+		Registry.register(Registry.ITEM, new Identifier("ccmagstripe", "card_reader"), new BlockItem(CARD_READER, new Item.Settings().group(ItemGroup.MISC)));
 	}
 }
